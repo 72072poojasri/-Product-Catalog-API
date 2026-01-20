c0c72739e675c5494d5f0acb890a9b59217e316d
72072poojasri <23p31a4242@acet.ac.in>
2026-01-20 22:54:52 +0530

    Add Redis caching and cache invalidation for products API

---
+++ config/redis.js
@@ -0,0 +1,19 @@
+const redis = require("redis");
+
+const client = redis.createClient({
+  url: process.env.REDIS_URL,
+});
+
+client.on("connect", () => {
+  console.log("Redis connected");
+});
+
+client.on("error", (err) => {
+  console.error("Redis error", err);
+});
+
+(async () => {
+  await client.connect();
+})();
+
+module.exports = client;
